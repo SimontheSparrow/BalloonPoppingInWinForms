9ad0bdae256e31e23dec638e95262e9ffff0b628
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2022-01-11 16:27:29 +0100

    Add files via upload

---
+++ Baloon popping in winforms/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Baloon_popping_in_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bomb = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bomb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score: 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Baloon_popping_in_winforms.Properties.Resources.redHeart;
+            this.pictureBox1.Location = new System.Drawing.Point(548, 294);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "balloon";
+            this.pictureBox1.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Baloon_popping_in_winforms.Properties.Resources.redStar;
+            this.pictureBox2.Location = new System.Drawing.Point(381, 147);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "balloon";
+            this.pictureBox2.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // bomb
+            // 
+            this.bomb.Image = global::Baloon_popping_in_winforms.Properties.Resources.bomb;
+            this.bomb.Location = new System.Drawing.Point(129, 121);
+            this.bomb.Name = "bomb";
+            this.bomb.Size = new System.Drawing.Size(100, 98);
+            this.bomb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bomb.TabIndex = 3;
+            this.bomb.TabStop = false;
+            this.bomb.Tag = "bomb";
+            this.bomb.Click += new System.EventHandler(this.KaBoom);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Baloon_popping_in_winforms.Properties.Resources.pinkBalloon;
+            this.pictureBox4.Location = new System.Drawing.Point(231, 394);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 143);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "balloon";
+            this.pictureBox4.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Baloon_popping_in_winforms.Properties.Resources.yellowBalloon;
+            this.pictureBox3.Location = new System.Drawing.Point(548, 147);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "balloon";
+            this.pictureBox3.Click += new System.EventHandler(this.PopBalloon);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1083, 618);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.bomb);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ballon pop";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bomb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox bomb;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Baloon popping in winforms/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baloon_popping_in_winforms
+{
+    public partial class Form1 : Form
+    {
+        int speed, score;
+        Random rand = new Random();
+        bool gameIsOver;
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            label1.Text = "Score: " + score;
+            foreach (Control item in Controls)
+            {
+              if (item is PictureBox)
+                {
+                    if ((string)item.Tag=="balloon" &&  bomb.Bounds.IntersectsWith(item.Bounds))
+                    {
+                        bomb.Top = rand.Next(700, 1200);
+                        bomb.Left = rand.Next(0, 500);
+                    }
+                    if ((string)item.Tag=="balloon" && item.Top<-80)
+                    {
+                        GameOver();
+                    }
+                    item.Top -= speed;
+                    if (item.Top< -100)
+                    {
+                        item.Top = rand.Next(700, 1200);
+                        item.Left = rand.Next(0, 500);
+                    }
+                }
+            }
+
+        }
+
+        private void PopBalloon(object sender, EventArgs e)
+        {
+            if (gameIsOver== false)
+            {
+                var balloon = (PictureBox)sender;
+                balloon.Top= rand.Next(700, 1200);
+                balloon.Left= rand.Next(0, 500);
+                score++;
+            }
+        }
+
+        private void KaBoom(object sender, EventArgs e)
+        {
+            bomb.Image = Properties.Resources.boom;
+            GameOver();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter && gameIsOver==true)
+            {
+                RestartGame();
+            }
+        }
+        private void RestartGame()
+        {
+            gameIsOver = false;
+            speed = 5;
+            score = 0;
+            label1.Text = "Score: " + score;
+            bomb.Image = Properties.Resources.bomb;
+            foreach (Control x in Controls)
+            {
+                if (x is PictureBox)
+                {
+                    x.Top = rand.Next(700, 1200);
+                    x.Left = rand.Next(0, 550);
+                }
+            }
+            gameTimer.Start();
+        }
+        private void GameOver() {
+            gameIsOver = true;
+            gameTimer.Stop();
+
+        }
+
+    }
+}
